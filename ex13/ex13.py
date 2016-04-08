2b0240a320d85991b7d3e050abb4ec1ea7dff77a
CAD Client <CAD Client>
2016-04-08 16:12:30 +0900

    ex13.py.001

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -3,7 +3,7 @@ from sys import argv
 print("argv = " + str(argv))
 script, first, second, third = argv
 
-print ("The script is called: " ,script)
-print ("Your first variable is: ", first)
-print ("Your second variable is: ", second)
-print ("Your third variable is: " ,third)
+print ("The script is called: %s" %script)
+print ("Your first variable is: %s" %first)
+print ("Your second variable is: %s" %second)
+print ("Your third variable is: %s" %third)
