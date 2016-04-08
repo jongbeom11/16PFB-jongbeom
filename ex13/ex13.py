01756a3c2c1129038391ef4623fef27070accd92
CAD Client <CAD Client>
2016-04-08 15:29:30 +0900

    ex13.py

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -1,5 +1,9 @@
 from sys import argv
 
-script, first, second, thrid = argv
+print("argv = " + str(argv))
+script, first, second, third = argv
 
-print "The script is called:",
+print ("The script is called: " ,script)
+print ("Your first variable is: ", first)
+print ("Your second variable is: ", second)
+print ("Your third variable is: " ,third)
