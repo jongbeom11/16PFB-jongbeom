e6d21c1beb1fdbda8d2d1e890bda38958fc43dae
CAD Client <CAD Client>
2016-04-15 15:16:33 +0900

    ex16.py

--- ex16/ex16.py
+++ ex16/ex16.py
@@ -2,14 +2,14 @@ from sys import argv
 
 script, filename = argv
 
-print "We're going to erase %r." $ filename
+print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN."
 
 raw_input("?")
 
-print "Openimg the file..."
-target = open(filename. 'w')
+print "Opening the file..."
+target = open(filename, 'w')
 
 print "Truncating the file.  Goodbye!"
 target.truncate()
@@ -30,4 +30,4 @@ target.write(line3)
 target.write("\n")
 
 print "And finally, we close it."
-target. close()
+target.close()
