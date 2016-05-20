194056f8789f2da6257bb68f457a640ab979e600
CAD Client <CAD Client>
2016-05-20 16:48:01 +0900

    1000이하의 3의배수 5의배수 숫자들 합 방법맞는것

--- euler/euler_0001.py
+++ euler/euler_0001.py
@@ -1,32 +1,12 @@
-#-*- coding: utf-8 -*-
-a = range(1,10)
-b = range(1,1000)
+#-*-coding:utf8
+A = [x for x in range(1, 1000) if x % 5 == 0] #5의 배수를 1에서 1000까지의 범위 내에서 찾습니다.
+print (A) #위의 결과값을 출력합니다.
 
-# 3 또는 5 의 배수 와 그의 합
-e = []
-for c in a :
-    if c%3 == 0 :
-        e.append(c)
-for d in a :
-    if d%5 == 0 :
-        e.append(d)
-print e
-f = sum(e)
-print(f)
+B = [x for x in range(1, 1000) if x % 3 == 0] #3의 배수를 1에서 1000까지의 범위 내에서 찾습니다
+print (B) #위의 결과값을 출력합니다.
 
-# 1000 미만 3 또는 5의 배수의 합
-g =[]
-for h in b :
-    if h%3 == 0 :
-        g.append(h)
-        # b에서 3으로 나누어서 나머지가 0인 숫자는 h 이다.
-        # g에 h 를 추가한다
-for i in b :
-    if i%5 == 0 :
-        g.append(i)
-        # b에서 5로 나누어서 나머지가 0인 숫자는 i 이다.
-        # g에 i 를 추가한다
-print g
-j = sum(g)
-# j는 g의 합
-print(j)
+C = [x for x in range(1, 1000) if x % 15 == 0] #3과 5의 최소 공배수인 15의 배수를 1에서 1000사이에서 찾습니다.
+print (C) #위의 결과값을 출력합니다.
+
+S = sum(A+B)-sum(C) #5의 배수와 3의 배수를 모두 더한후 2번 더해진 3과 5의 최소 공배수의 배수 즉, 15의 배수를 한번 빼줍니다.
+print (S) #위의 값을 출력해 줍니다.
